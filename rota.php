3e16d16c4f692f92f4b68e78a279a750674635d2
Igor Camargo <igorfelcam@hotmail.com>
2016-05-02 09:29:46 -0300

    aumentando eras

--- rota.php
+++ rota.php
@@ -72,7 +72,7 @@
 				<option value="50">50</option>
 				<option value="100">100</option>
 				<option value="200">200</option>
-				<option value="300">300</option>
+				<option value="400">400</option>
 			</select>
 
 			<div class="btnCenter">
